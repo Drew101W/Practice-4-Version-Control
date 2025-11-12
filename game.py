b2cbfd901b2b62ba25b351f7d2200f71b9a25690
Drew101W <dwhiting9877@floridapoly.edu>
2025-11-12 20:25:26 +0000

    Drew : add player input, validation, and deciding winner

--- game.py
+++ game.py
@@ -7,3 +7,24 @@ while player not in ("rock", "paper", "scissors"):
     player = input("Invalid choice. Choose rock, paper, or scissors: ").strip().lower()
 
 print("Player chose:", player)
+
+#Computer's choice 
+# ------------------------------------------
+
+
+
+
+
+
+#------------------------------------------
+
+
+# decide winner
+if player == computer:
+    print("It's a tie!")
+elif (player == "rock" and computer == "scissors") or \
+     (player == "paper" and computer == "rock") or \
+     (player == "scissors" and computer == "paper"):
+    print("Player wins!")
+else:
+    print("Computer wins!")
